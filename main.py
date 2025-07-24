850905b25a94a0f2d3226d515397e7a339cc9e8a
Jenikh <164548559+Jenikh@users.noreply.github.com>
2025-07-24 16:01:44 +0200

    Aktualizovat pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
-import string
-def select(letters=True):
-    if letters:
-        simbols = 
+from microbit import *
+
+def select(printable=False, letters=True, only_lowercase=False, only_uppercase=False):
+    ascii_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ascii_lowercase = "abcdefghijklmnopqrstuvwxyz"
+    ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digits = "0123456789"
+    printable_chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"
+
+    if printable:
+        s = printable_chars
+    elif letters:
+        if only_lowercase:
+            s = ascii_lowercase
+        elif only_uppercase:
+            s = ascii_uppercase
+        else:
+            s = ascii_letters
+    else:
+        s = digits
+
+    symbols = list(s)
+
+    index = 0
+    while True:
+        letter = symbols[index]
+        basic.show_string(letter)
+
+        # Wait for button press
+        while True:
+            if button_b.is_pressed():
+                return letter  # Return current letter when Button B pressed
+            if button_a.is_pressed():
+                # Move to next letter on Button A press
+                index = (index + 1) % len(symbols)
+                break  # Exit inner while loop to show next letter
+            sleep(100)  # Small delay to avoid busy waiting
+
+# Example usage
+selected_letter = select(printable=False, letters=True, only_lowercase=True)
+display.scroll("You picked: {}".format(selected_letter))
--- main.ts
+++ main.ts
@@ -1,8 +1,44 @@
-function select(letters: boolean = true) {
-    let simbols: any[];
-    if (letters) {
-        simbols = []
+function select(printable: boolean = false, letters: boolean = true, only_lowercase: boolean = false, only_uppercase: boolean = false): string {
+    let s: string;
+    let letter: string;
+    let ascii_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let ascii_lowercase = "abcdefghijklmnopqrstuvwxyz"
+    let ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let digits = "0123456789"
+    let printable_chars = `0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#\$%&'()*+,-./:;<=>?@[\\]^_\`\{|\}~ 	
+`
+    if (printable) {
+        s = printable_chars
+    } else if (letters) {
+        if (only_lowercase) {
+            s = ascii_lowercase
+        } else if (only_uppercase) {
+            s = ascii_uppercase
+        } else {
+            s = ascii_letters
+        }
+        
+    } else {
+        s = digits
     }
     
+    let symbols = []
+    for (letter of s) {
+        symbols.push(letter)
+    }
+    //  Show each letter on the Micro:bit display
+    while (true) {
+        for (letter of symbols) {
+            while (!input.buttonIsPressed(Button.A)) {
+                basic.showString(letter)
+            }
+            if (input.buttonIsPressed(Button.B)) {
+                return letter
+            }
+            
+        }
+    }
 }
 
+//  Example usage
+select(false, true, true)
